08b131121bc39a1d68a81cd737307db7c1adc859
Robson Locatelli <rlocatelli9@gmail.com>
2018-04-07 11:19:12 -0300

    criação de rotas get e post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +14,37 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * quando for necessário criar rotas onde algum argumento seja opcional (colocando ?), 
+ * é necessário criar um valor padrão caso o argumento não seja passado.
+ */
+Route::get('rota/{id}/{name?}', function ($id, $name = "Usuário"){
+    return "Cliente $id, $name";
+})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
+
+Route::get('client', function(){
+   $token = csrf_token();
+   $action = route('client.store');
+    $html = <<<HTML
+    <html>
+        <body>
+            <form method="POST" action="$action">
+                <input type="hidden" name="_token" value="$token" >
+                <input type="text" name="value"/>
+                <button type="submit">Enviar</submit>
+            </form>
+        </body>
+    </html>
+HTML;
+
+    return $html;
+});
+
+/**
+ * rotas para fazer uma requisição POST
+ */
+
+Route::post('cliente', function(Request $request){
+    return $request->get('value');
+})->name('client.store');
